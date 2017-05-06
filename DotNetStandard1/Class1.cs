9270e68531434ecf386bc8ef6ce4c7c5fcd1a447
appveyor <appveyor>
2017-05-06 03:05:04 +0000

    initial commit

---
+++ DotNetStandard1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetStandard1
+{
+    public class Class1
+    {
+        public void DoSomth()
+        {
+            Console.WriteLine("somth");
+        }
+    }
+}
